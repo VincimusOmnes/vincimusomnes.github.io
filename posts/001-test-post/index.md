864b6cd187878d1f12681890745d4485f62d33ce
Vincimus Omnes <VincimusOmnes@proton.me>
2025-10-20 08:29:07 +1300

    Created a test blog post

---
+++ posts/001-test-post/index.md
@@ -0,0 +1,3 @@
+## File is loaded
+
+![STS wallpaper](/sts-wallpaper-3.png "sts wallpaper")
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,77 @@
-const getMarkdown = () => {
-    fetch('../test.md')
-    .then(response => response.text())
-    .then(text => {
-        // Process the Markdown text here
-        console.log(text);
-    })
-    .catch(error => console.error('Error fetching the Markdown file:', error));
+const getFile = async (filePath) => {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+        // Return promise resolving to text content
+        return await response.text();
+    } catch (error) {
+        console.error('Error fetching file:', error);
+        throw error;
+    }
 };
 
+// Okay so the obvious issue with this file layout
+// Is that getting the posts folders is difficult
+// Obviously I don't want to hardcode each post path
+
+// We want to make adding posts as simple as possible.
+// We want to keep everything organised
+
+// Is there a way to get all files in a directory with JS?
+// Not really, due to security reasons.
+// We could have a published posts file that lists all posts.
+// That would allow us to write posts in advance and publish
+// by adding them to that file.
+
+// We could probably write a github action to post new posts.
+
+// Is it better to have a key value pair with path and
+// published status?
+
+// Is there any point using markdown instead of HTML directly?
+
 console.log("JavaScript is working!");
-getMarkdown();
+
+const displayAllPosts = () => {
+    getFile('posts/published-posts.json').then(data => {
+        const publishedPosts = JSON.parse(data)["published-posts"];
+        publishedPosts.forEach(post => {
+            const index = post.path + "/index.md";
+            getFile(`posts/${index}`).then(markdown => {
+                console.log(markdown);
+                displayPost(markdownToHtml(markdown, post.path));
+            });
+        });
+    });
+}
+
+const displayPost = (html) => {
+    const postContainer = document.createElement('div');
+    postContainer.className = 'post';
+    postContainer.innerHTML = html;
+    document.body.appendChild(postContainer);
+}
+
+const markdownToHtml = (markdown, path) => {
+    let html = markdown;
+    // This needs to be escaped if open to untrusted input
+    html = html.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    html = html.replace(/^## (.*$)/gim, '<h2>$1</h2>');
+    html = html.replace(/^### (.*$)/gim, '<h3>$1</h3>');
+    html = html.replace(/^#### (.*$)/gim, '<h4>$1</h4>');
+    html = html.replace(/^##### (.*$)/gim, '<h5>$1</h5>');
+    html = html.replace(/^###### (.*$)/gim, '<h6>$1</h6>');
+    // ![Alt text](url "a title")
+    const imgReg = /!\[([^\]]*)\]\(([^)\s]+)(?:\s+"([^"]+)")?\)/g;
+    html = html.replace(imgReg, `<img src="posts/${path}/images/$2" alt="$1" title="$3">`);
+    // Links
+    // Text styles
+    // Syntax highlighting
+    // Tables
+    // Lists
+    // Escape characters
+    
+    return html;
+}
+
+displayAllPosts()
